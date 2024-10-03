269fb0cd747dffb2c2babfc26a6147deae4c5891
KingsSack <sanjaybrink@gmail.com>
2024-10-03 14:13:41 -0700

    Add NullOp

---
+++ TeamCode/src/main/kotlin/NullOp.kt
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.util.ElapsedTime
+
+@TeleOp(name = "Manual - NullOp", group = "Competition")
+class NullOp : OpMode() {
+    private val runtime = ElapsedTime()
+
+    override fun init() {
+        telemetry.addData("Status", "Initialized")
+    }
+
+    override fun loop() {
+        telemetry.addData("Status", "Run Time: $runtime")
+        telemetry.update()
+    }
+
+    override fun start() {
+        runtime.reset()
+    }
+}
