3eeb355cbae7be3231507fc0bc8e44040f73558f
KingsSack <sanjaybrink@gmail.com>
2024-12-13 10:38:36 -0800

    Better robot tuning

--- TeamCode/src/main/kotlin/rr/TuningOpModes.kt
+++ TeamCode/src/main/kotlin/rr/TuningOpModes.kt
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode
+package org.firstinspires.ftc.teamcode.rr
 
 import com.acmerobotics.dashboard.FtcDashboard
 import com.acmerobotics.dashboard.config.reflection.ReflectionConfig
