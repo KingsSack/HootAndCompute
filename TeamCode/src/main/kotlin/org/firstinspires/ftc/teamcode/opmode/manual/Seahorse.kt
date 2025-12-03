9dd129c613ce08a8b6b87ecc357e619380747908
KingsSack <sanjaybrink@gmail.com>
2025-12-03 09:39:29 -0800

    feat: Seahorse

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/manual/Seahorse.kt
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.opmode.manual
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import dev.kingssack.volt.attachment.drivetrain.MecanumDriveWithPP
+import dev.kingssack.volt.opmode.manual.SimpleManualModeWithSpeedModes
+import dev.kingssack.volt.util.GamepadAnalogInput
+import dev.kingssack.volt.util.GamepadButton
+import org.firstinspires.ftc.teamcode.robot.JonesPP
+
+@TeleOp(name = "Seahorse", group = "Competition")
+class Seahorse :
+    SimpleManualModeWithSpeedModes<MecanumDriveWithPP, JonesPP>({ hardwareMap ->
+        JonesPP(hardwareMap)
+    }) {
+    init {
+        onButtonTapped(GamepadButton.RIGHT_BUMPER2) { +robot.launcher.enable() }
+        onButtonReleased(GamepadButton.RIGHT_BUMPER2) { +robot.launcher.disable() }
+        onAnalog(GamepadAnalogInput.RIGHT_TRIGGER2) { value ->
+            if (!isButtonPressed(GamepadButton.RIGHT_BUMPER2)) {
+                robot.launcher.setPower(value.toDouble())
+            }
+        }
+    }
+}
