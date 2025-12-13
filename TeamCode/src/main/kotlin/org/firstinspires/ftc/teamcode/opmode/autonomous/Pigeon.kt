f17030d7ca9e1e167d31f8183db9058696fde56e
KingsSack <sanjaybrink@gmail.com>
2025-12-12 16:39:49 -0800

    fix: Incorrect initial heading

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/autonomous/Pigeon.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/autonomous/Pigeon.kt
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.util.AllianceColor
 import org.firstinspires.ftc.teamcode.util.PathConstants
 
 @Autonomous(name = "Pigeon", group = "Competition", preselectTeleOp = "Manatee")
-class Pigeon : AutonomousMode<GabePP>({ hardwareMap -> GabePP(hardwareMap, Pose(57.0, 9.0, 0.0)) }) {
+class Pigeon : AutonomousMode<GabePP>({ hardwareMap -> GabePP(hardwareMap, Pose(57.0, 9.0, 90.0)) }) {
     private val paths by lazy { PathConstants(robot.drivetrain.follower, AllianceColor.BLUE) }
 
     override fun sequence() = execute { +robot.drivetrain.pathTo(paths.pathOffWallLaunchLine) }
