2a7e543c7fe5f95b37ba77379e4593f1bad7e08c
LegoBrainBiker <sameer.brink26@scdsstudent.org>
2026-01-04 12:57:13 -0800

    feat: add error handling to hopefuly prevent future crash loops

--- Volt/src/main/kotlin/dev/kingssack/volt/opmode/VoltOpMode.kt
+++ Volt/src/main/kotlin/dev/kingssack/volt/opmode/VoltOpMode.kt
@@ -1,12 +1,14 @@
 package dev.kingssack.volt.opmode
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.hardware.HardwareMap
 import dev.frozenmilk.sinister.Scanner.Companion.INDEPENDENT
 import dev.frozenmilk.sinister.sdk.opmodes.OpModeScanner
 import dev.frozenmilk.sinister.sdk.opmodes.OpModeScanner.RegistrationHelper
 import dev.frozenmilk.sinister.targeting.NarrowSearch
 import dev.kingssack.volt.robot.Robot
+import org.firstinspires.ftc.robotcore.internal.opmode.OpModeMeta
 import java.lang.reflect.Modifier
 
 abstract class VoltOpMode<R : Robot>(robotFactory: (HardwareMap) -> R) : LinearOpMode() {
@@ -35,9 +37,17 @@ abstract class VoltOpMode<R : Robot>(robotFactory: (HardwareMap) -> R) : LinearO
         override val unloadAdjacencyRule = INDEPENDENT
         override val targets = NarrowSearch()
         override fun scan(loader: ClassLoader, cls: Class<*>, registrationHelper: RegistrationHelper) {
-            if (VoltOpMode::class.java.isAssignableFrom(cls) && !Modifier.isAbstract(cls.modifiers))
-                (cls as Class<VoltOpMode<*>>).getDeclaredConstructor().newInstance().register(registrationHelper)
-            // due to type erasure, casting is necessary here
+            try {
+                if (VoltOpMode::class.java.isAssignableFrom(cls) && !Modifier.isAbstract(cls.modifiers))
+                    (cls as Class<VoltOpMode<*>>).getDeclaredConstructor().newInstance().register(registrationHelper)
+                // due to type erasure, casting is necessary here
+            } catch (e : Error) {
+                registrationHelper.register(OpModeMeta.Builder().setName("error: $e").build(), object : OpMode() {
+                    override fun init() {}
+                    override fun loop() {}
+                })
+                // create an opmode with the error in its name
+            }
         }
     }
 }
