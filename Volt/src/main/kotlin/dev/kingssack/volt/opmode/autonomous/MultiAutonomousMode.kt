1c98db59703d9d6e490bc38cae33b1ee3dcec6b1
LegoBrainBiker <sameer.brink26@scdsstudent.org>
2026-01-12 10:22:41 -0800

    fix: delete this

--- Volt/src/main/kotlin/dev/kingssack/volt/opmode/autonomous/MultiAutonomousMode.kt
+++ Volt/src/main/kotlin/dev/kingssack/volt/opmode/autonomous/MultiAutonomousMode.kt
@@ -5,16 +5,9 @@ import dev.kingssack.volt.robot.Robot
 import org.firstinspires.ftc.robotcore.internal.opmode.OpModeMeta
 
 abstract class MultiAutonomousMode<R : Robot, E: Enum<E>>(val enumClass: Class<E>) : AutonomousMode<R>() {
-    private fun construct(enum: E) : MultiAutonomousMode<R, E> {
-        return try {
-            javaClass.getDeclaredConstructor(AllianceColor::class.java).newInstance(enum)
-        } catch (_: NoSuchMethodException) {
-            javaClass.getDeclaredConstructor().newInstance()
-        }
-    }
     override fun register(registrationHelper: RegistrationHelper) {
         enumClass.enumConstants!!.forEach { value -> {
-            val instance : MultiAutonomousMode<R, E> = construct(value)
+            val instance : MultiAutonomousMode<R, E> = javaClass.getDeclaredConstructor().newInstance()
             instance.type = value
             registrationHelper.register(OpModeMeta.Builder().setName("$name "+value.name).setGroup(group).setFlavor(OpModeMeta.Flavor.AUTONOMOUS).setTransitionTarget(autoTransition).setSource(OpModeMeta.Source.EXTERNAL_LIBRARY).build(), instance)
         } }
