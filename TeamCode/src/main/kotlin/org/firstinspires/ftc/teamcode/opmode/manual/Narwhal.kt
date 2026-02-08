efdef21220db7249990f2411634ba4dee47c26bf
KingsSack <sanjaybrink@gmail.com>
2026-02-08 14:45:49 -0800

    refactor: Update types

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/manual/Narwhal.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/manual/Narwhal.kt
@@ -3,10 +3,11 @@ package org.firstinspires.ftc.teamcode.opmode.manual
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import dev.kingssack.volt.attachment.drivetrain.MecanumDriveWithPP
 import dev.kingssack.volt.opmode.manual.SimpleManualModeWithSpeedModes
+import org.firstinspires.ftc.teamcode.robot.Gabe
 import org.firstinspires.ftc.teamcode.robot.GabePP
 
 @TeleOp(name = "Narwhal", group = "Default")
-class Narwhal : SimpleManualModeWithSpeedModes<MecanumDriveWithPP, GabePP>({ GabePP(it) }) {
+class Narwhal : SimpleManualModeWithSpeedModes<MecanumDriveWithPP, Gabe<MecanumDriveWithPP>>({ GabePP(it) }) {
     override fun initialize() {
         super.initialize()
         robot.drivetrain.startTeleOpDrive()
