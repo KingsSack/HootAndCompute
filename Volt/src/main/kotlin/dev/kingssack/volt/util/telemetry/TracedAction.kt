47ec0d6150235e46c5c700b938f0456f39d53754
KingsSack <sanjaybrink@gmail.com>
2026-02-09 11:38:16 -0800

    fix(Volt): Track end time

--- Volt/src/main/kotlin/dev/kingssack/volt/util/telemetry/TracedAction.kt
+++ Volt/src/main/kotlin/dev/kingssack/volt/util/telemetry/TracedAction.kt
@@ -3,27 +3,46 @@ package dev.kingssack.volt.util.telemetry
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket
 import com.acmerobotics.roadrunner.Action
 
+/**
+ * TracedAction is a wrapper around an Action that tracks its execution time and reports it to an ActionTracer.
+ *
+ * @param label A human-readable label for the action, used in telemetry
+ * @param inner The actual Action being wrapped and executed
+ * @param trace The ActionTracer instance that will receive updates about this action's execution
+ */
 class TracedAction(
     val label: String,
     private val inner: Action,
     private val trace: ActionTracer = ActionTracer,
 ) : Action {
-    private var startTime: Long? = null
+    var startTime: Long? = null
+        private set
 
-    val elapsedMs
-        get() = startTime?.let { System.currentTimeMillis() - it } ?: 0L
+    var endTime: Long? = null
+        private set
+
+    val elapsedMs: Long
+        get() =
+            when {
+                startTime == null -> 0L
+                endTime != null -> (endTime!! - startTime!!) / 1_000_000
+                else -> (System.nanoTime() - startTime!!) / 1_000_000
+            }
 
     private var initialized = false
 
     override fun run(p: TelemetryPacket): Boolean {
         if (!initialized) {
-            startTime = System.currentTimeMillis()
+            startTime = System.nanoTime()
             trace.markRunning(this)
             initialized = true
         }
 
         val running = inner.run(p)
-        if (!running) trace.markCompleted(this)
+        if (!running) {
+            endTime = System.nanoTime()
+            trace.markCompleted(this)
+        }
         return running
     }
 }
