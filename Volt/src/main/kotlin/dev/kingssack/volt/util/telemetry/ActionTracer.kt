66fec9df6e545b3582570eed24d4daf734e789ab
KingsSack <sanjaybrink@gmail.com>
2026-02-09 11:37:56 -0800

    feat(Volt): Add MAX_TRACED_ACTIONS

--- Volt/src/main/kotlin/dev/kingssack/volt/util/telemetry/ActionTracer.kt
+++ Volt/src/main/kotlin/dev/kingssack/volt/util/telemetry/ActionTracer.kt
@@ -1,16 +1,20 @@
 package dev.kingssack.volt.util.telemetry
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket
-import dev.kingssack.volt.util.telemetry.TracedAction
-import org.firstinspires.ftc.robotcore.external.Telemetry
 import java.util.concurrent.ConcurrentHashMap
+import org.firstinspires.ftc.robotcore.external.Telemetry
 
 object ActionTracer {
+    private const val MAX_TRACE_SIZE = 100
     private val trace = mutableListOf<TracedAction>()
     private val running = ConcurrentHashMap.newKeySet<TracedAction>()
 
     fun markRunning(action: TracedAction) {
         trace.add(action)
+        if (trace.size > MAX_TRACE_SIZE) {
+            trace.removeAt(0)
+        }
+
         running.add(action)
     }
 
@@ -27,15 +31,20 @@ object ActionTracer {
             if (running.isEmpty()) {
                 addLine("None")
             } else {
-                running.forEachIndexed { i, action ->
-                    addData("[$i]", "${action.label} (${action.elapsedMs}ms")
-                }
+                running
+                    .toList()
+                    .sortedBy { it.startTime }
+                    .forEachIndexed { i, action ->
+                        addData("[$i]", "${action.label} (${action.elapsedMs}ms)")
+                    }
             }
         }
     }
 
     context(packet: TelemetryPacket)
     fun writePacket() {
-        trace.forEach { packet.put("action/${it.label}", it.elapsedMs) }
+        trace.forEachIndexed { i, action ->
+            packet.put("action/$i/${action.label}", action.elapsedMs)
+        }
     }
 }
