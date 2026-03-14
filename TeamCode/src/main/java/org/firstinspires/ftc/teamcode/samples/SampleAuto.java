5ba9a9fc3473d0396d9dfb2dd4100686bb22665a
KingsSack <sanjaybrink@gmail.com>
2026-03-13 17:34:46 -0700

    feat(Volt): Up-to-date sample code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/samples/SampleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/samples/SampleAuto.java
@@ -1,28 +1,20 @@
-//package org.firstinspires.ftc.teamcode.samples;
-//
-//import com.acmerobotics.roadrunner.Action;
-//import com.acmerobotics.roadrunner.SequentialAction;
-//import dev.kingssack.volt.opmode.autonomous.AutonomousMode;
-//import org.jetbrains.annotations.NotNull;
-//
-//public class SampleAuto extends AutonomousMode<SampleRobot> {
-//    public SampleAuto() {
-//        super(SampleRobot::new);
-//    }
-//
-//    Action sampleAction() {
-//        return new SequentialAction(
-//                super.getRobot().motor.goTo(0.5, 50),
-//                super.getRobot().motor.goTo(0.5, 0),
-//                super.getRobot().motor.goTo(0.5, 100)
-//        );
-//    }
-//
-//    @Override
-//    public void sequence(@NotNull SampleRobot robot) {
-//        execute(builder -> {
-//            builder.unaryPlus(sampleAction());
-//            return kotlin.Unit.INSTANCE;
-//        });
-//    }
-//}
+package org.firstinspires.ftc.teamcode.samples;
+
+import dev.kingssack.volt.opmode.autonomous.AutonomousMode;
+import dev.kingssack.volt.util.Event.AutonomousEvent.Start;
+import kotlin.Unit;
+
+@SuppressWarnings("unused")
+public class SampleAuto extends AutonomousMode<SampleRobot> {
+    public SampleAuto() {
+        super(SampleRobot::new);
+    }
+
+    @Override
+    public void defineEvents() {
+        then(Start.INSTANCE, builder -> {
+            builder.unaryPlus(getRobot().score());
+            return Unit.INSTANCE;
+        });
+    }
+}
