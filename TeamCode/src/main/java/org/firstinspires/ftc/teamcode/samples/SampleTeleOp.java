2b00cbcd62f14c400de893d3f4fbca048a740244
KingsSack <sanjaybrink@gmail.com>
2026-03-19 12:54:30 -0700

    refactor(Volt): Make more like SampleAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/samples/SampleTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/samples/SampleTeleOp.java
@@ -1,27 +1,27 @@
 package org.firstinspires.ftc.teamcode.samples;
 
-import dev.kingssack.volt.opmode.VoltOpModeMeta;
 import dev.kingssack.volt.opmode.manual.ManualMode;
 import dev.kingssack.volt.util.Event.ManualEvent.*;
 import dev.kingssack.volt.util.buttons.Button;
 import kotlin.Unit;
 import org.jetbrains.annotations.NotNull;
 
-// an actual opmode would not have abstract
-@VoltOpModeMeta(name = "sample opmode name")
+// An actual opmode would not be abstract
 @SuppressWarnings("unused")
 abstract public class SampleTeleOp extends ManualMode<SampleRobot> {
+    @NotNull SampleRobot robot = new SampleRobot(getHardwareMap());
+
+    @Override
+    protected @NotNull SampleRobot getRobot() {
+        return robot;
+    }
+
     public SampleTeleOp() {
         super(new ManualParams());
+
         then(new Tap(Button.A1), builder -> {
             builder.unaryPlus(getRobot().sampleAttachment.exampleAction());
             return Unit.INSTANCE;
         });
-    }
-    SampleRobot robot = new SampleRobot(getHardwareMap());
-    @NotNull
-    @Override
-    protected SampleRobot getRobot() {
-        return robot;
     }
 }
