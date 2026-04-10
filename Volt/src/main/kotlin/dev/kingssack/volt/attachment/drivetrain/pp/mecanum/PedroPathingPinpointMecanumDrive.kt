5c6a24b6d51fe0fccdae27fd4512c5615125eee0
KingsSack <sanjaybrink@gmail.com>
2026-04-10 11:15:55 -0700

    feat(Volt): PedroPathing Pinpoint localizer

---
+++ Volt/src/main/kotlin/dev/kingssack/volt/attachment/drivetrain/pp/mecanum/PedroPathingPinpointMecanumDrive.kt
@@ -0,0 +1,38 @@
+package dev.kingssack.volt.attachment.drivetrain.pp.mecanum
+
+import com.pedropathing.follower.FollowerConstants
+import com.pedropathing.ftc.FollowerBuilder
+import com.pedropathing.ftc.drivetrains.MecanumConstants
+import com.pedropathing.ftc.localization.constants.PinpointConstants
+import com.pedropathing.geometry.Pose
+import com.pedropathing.paths.PathConstraints
+import com.qualcomm.robotcore.hardware.HardwareMap
+import dev.kingssack.volt.attachment.drivetrain.pp.PedroPathingDrivetrain
+
+/**
+ * A mecanum [dev.kingssack.volt.attachment.drivetrain.Drivetrain] integrated with the PedroPathing
+ * library.
+ *
+ * @param hardwareMap The FTC hardware map
+ * @param followerConstants constants for the path follower
+ * @param localizerConstants constants for the pinpoint localizer
+ * @param pathConstraints constraints for path following
+ * @param driveConstants constants specific to the mecanum drivetrain
+ * @param initialPose the robot's initial pose
+ */
+class PedroPathingPinpointMecanumDrive(
+    hardwareMap: HardwareMap,
+    followerConstants: FollowerConstants,
+    localizerConstants: PinpointConstants,
+    pathConstraints: PathConstraints,
+    driveConstants: MecanumConstants,
+    initialPose: Pose = Pose(),
+) :
+    PedroPathingDrivetrain(
+        FollowerBuilder(followerConstants, hardwareMap)
+            .pinpointLocalizer(localizerConstants)
+            .pathConstraints(pathConstraints)
+            .mecanumDrivetrain(driveConstants)
+            .build(),
+        initialPose,
+    )
